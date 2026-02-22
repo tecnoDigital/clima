06064051b978c2ce21ca3040ee926c692879a93c
tecnoDigital <149269689+tecnoDigital@users.noreply.github.com>
2026-02-21 20:21:47 -0600

    Update app.py
    Signed-off-by: tecnoDigital <149269689+tecnoDigital@users.noreply.github.com>

--- app.py
+++ app.py
@@ -13,6 +13,10 @@ def ejecutar():
     respuesta = mi_logica_python()
     return jsonify(resultado=respuesta)
 
+@app.get("/health")
+def health():
+    return {"status": "ok"}, 200
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
 
